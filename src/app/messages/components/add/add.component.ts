24350c4c91af61336d66dec7a6b8ca7ec8d69e88
Ezequiel Jaramillo <ziquij96@gmail.com>
2018-12-28 23:06:09 -0500

    servicio de mensajes. Componente de envio de mensajes. Enviar Mensajes

--- src/app/messages/components/add/add.component.ts
+++ src/app/messages/components/add/add.component.ts
@@ -1,20 +1,73 @@
 import { Component, OnInit, DoCheck } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../../modules/user';
+import { Follow } from '../../../modules/follow';
+import { Message } from '../../../modules/message';
+import { UserService } from '../../../services/user.service';
+import { MessageService } from '../../../services/message.service';
+import { FollowService } from '../../../services/follow.service';
+import { GLOBAL } from '../../../services/global';
 
 @Component({
     selector: 'add',
-    templateUrl: './add.component.html'
+    templateUrl: './add.component.html',
+    providers: [FollowService, MessageService]
 })
 
 export class AddComponent implements OnInit {
     
     public title:string;
+    public message: Message;
+    public identity;
+    public token;
+    public url: string;
+    public status: string;
+    public follows;
 
-    public constructor(){
+    public constructor(
+        private _route: ActivatedRoute,
+        private _router: Router,
+        private _followService: FollowService,
+        private _messageService: MessageService,
+        private _userService: UserService
+    ){
         this.title = 'Enviar mensaje';
+        this.identity = this._userService.getIdentity();
+        this.token = this._userService.getToken();
+        this.url = GLOBAL.url;
+        this.message = new Message('', '', '', '', this.identity._id, '');
     }
 
     ngOnInit(){
         console.log('Componente de add cargado ...');
+        this.getMyFollows();
+    }
+
+    onSubmit(form){
+        this._messageService.addMessage(this.token, this.message).subscribe(
+            response => {
+                if(response.message){
+                    this.status = 'success';
+                    form.reset();
+                }
+                
+            },
+            error => {
+                this.status = 'error';
+                console.log(<any>error)
+            }
+        );
+    }
+
+    getMyFollows(){
+        this._followService.getMyFollows(this.token).subscribe(
+            response => {
+                this.follows = response.follows;
+            },
+            error => {
+                console.log(<any>error)
+            }
+        );
     }
 }
   
---
+++ src/app/services/message.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Message } from '../modules/message';
+import { GLOBAL } from './global';
+
+@Injectable()
+export class MessageService{
+    public url: string;
+
+    constructor(private _http: HttpClient){
+        this.url = GLOBAL.url;
+    }
+
+    addMessage(token, message): Observable<any>{
+        let params = JSON.stringify(message);
+        let headers = new HttpHeaders().set('Content-Type', 'application/json')
+                                        .set('Authorization', token);
+        
+        return this._http.post(this.url + 'message', params, {headers: headers});
+    }
+
+    //obtener mensajes recibidos
+    getMyMessages(token, page = 1): Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/json')
+                                        .set('Authorization', token);
+        
+        return this._http.get(this.url + 'my-messages/' + page, {headers: headers});
+    }
+
+    //obtener mensajes enviados
+    getEmmitMessages(token, page = 1): Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/json')
+                                        .set('Authorization', token);
+        
+        return this._http.get(this.url + 'messages/' + page, {headers: headers});
+    }
+
+}
